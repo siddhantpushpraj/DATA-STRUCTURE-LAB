6ea889d0c37007f38287610d87ac0aaf253a5521
siddhantpushpraj <36290434+siddhantpushpraj@users.noreply.github.com>
2018-10-14 13:20:50 +0530

    Add files via upload

---
+++ exp2.cpp
@@ -0,0 +1,64 @@
+/*
+							EXPERIMENT 2
+	Write a program to accept N numbers from the user and store them in an array. 
+	Then, accept another number from the user and search that using Linear Search
+*/
+#include<iostream>
+using namespace std;
+class lsearch
+{                 
+int a[10],limit,item,n,flag;
+public:
+	int i;
+void read()
+{
+cout<<" Linear Search  "<<endl;
+cout<<"enter  size   ";
+cin>>limit;
+cout<<endl;
+cout<<"element of araray"<<endl;
+for(i=0;i<limit;i++)
+{
+cin>>a[i];
+}
+cout<<" Enter the element to be searched:  ";
+cin>>item;
+cout<<endl;
+}
+void search()
+{
+for(i=0;i<limit;i++)
+{
+if(a[i]==item)
+{
+flag=1;
+break;
+}
+else
+flag=0;
+}
+if(flag==1)
+cout<<"The item found at index   "<<i<<"  "<<endl;
+else
+cout<<" Sorry!The element was not found ";
+}
+void display()
+{
+cout<<"OUTPUT "<<endl;
+cout<<"The array is: "<<endl;
+for(i=0;i<limit;i++)
+{
+cout<<a[i]<<"   ";
+}
+cout<<endl;
+}
+};
+int main()
+{
+lsearch ls;
+ls.read();
+
+ls.search();
+ls.display();
+
+}
