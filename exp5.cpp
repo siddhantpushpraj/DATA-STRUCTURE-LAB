b761cc0d52346b9421afbe15db4671b96a5226ae
siddhantpushpraj <36290434+siddhantpushpraj@users.noreply.github.com>
2018-10-14 15:46:09 +0530

    Update exp5.cpp

--- exp5.cpp
+++ exp5.cpp
@@ -1,3 +1,12 @@
+/*
+				Experiment 5
+Write a menu driven program that implements the following operations on a   
+Stack(either implement as Linear array and as Linked list) :
+•	Push 
+•	Pop  
+•	Display Top of the stack
+
+*/
 #include<iostream>
 using namespace std;
 #define size 10
