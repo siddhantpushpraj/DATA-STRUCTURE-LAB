cd2ce832adda1367299550d1d6dec01f1168af48
siddhantpushpraj <36290434+siddhantpushpraj@users.noreply.github.com>
2018-10-14 15:40:19 +0530

    Update exp4.cpp

--- exp4.cpp
+++ exp4.cpp
@@ -1,3 +1,16 @@
+/*
+
+			Experiment 4
+Write a menu driven program that implements the following operations on a   
+doubly and Circular linked list :
+•	Insert a new element at the beginning ,end and in-between the given list
+•	Delete an existing element
+•	Search an element
+•	Display all the elements 
+
+*/
+
+
 #include<iostream>
 using namespace std;
 struct node
